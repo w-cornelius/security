febe11f3733b67bffbf2f701c865c6616add1a42
w-cornelius <74280353+w-cornelius@users.noreply.github.com>
2025-07-30 18:57:04 -0400

    Update download.js

--- functions/download.js
+++ functions/download.js
@@ -1,18 +1,22 @@
 // functions/download.js
 
-// Import the static file from your assets folder.
-// Make sure the path and filename match what you uploaded.
-import fileToDownload from '../assets/dubc_io-email-pgp-pubkey.asc';
-
 export async function onRequest(context) {
-  // Create a response from the imported file data.
-  const response = new Response(fileToDownload.stream());
+  // This is the path to your file within your project's output directory.
+  // Since your file is in the 'assets' folder at the root, the URL path is '/assets/...'
+  const fileUrl = new URL('/assets/dubc_io-email-pgp-pubkey.asc', context.request.url);
 
-  // Set the headers to tell the browser to download the file.
+  // Use context.env.ASSETS.fetch() to get the static file from your deployment.
+  const asset = await context.env.ASSETS.fetch(fileUrl);
+
+  // Create a new response from the asset's body.
+  // We clone the original headers to preserve things like ETag for caching.
+  const response = new Response(asset.body, asset);
+
+  // Set the headers to tell the browser to download the file instead of displaying it.
   response.headers.set('Content-Type', 'application/octet-stream');
   response.headers.set(
     'Content-Disposition',
-    'attachment; filename="dubc_io-email-pgp-pubkey.asc"' // This is the name the user will see.
+    'attachment; filename="dubc_io-email-pgp-pubkey.asc"'
   );
 
   return response;
